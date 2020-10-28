10c0fe866c23b42d0fb0fa5712cd0a038e646b40
butterfly1of4 <jhouck29@gmail.com>
2020-10-28 19:46:44 -0400

    omg seeded

--- capitals/models.py
+++ capitals/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class States(models.Model):
-    name= models.CharField(max_length=200, default="name")
-    capital= models.CharField(max_length= 300, default="capital")
+    state_name= models.CharField(max_length=200, default="name")
+    state_capital= models.CharField(max_length= 300, default="capital")
     
     def __str__(self):
-        return self.name
+        return self.state_name
